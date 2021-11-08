4876ba42ed318fca2b1553ddd5f3b8308e8a4f04
amyjprogrammer <amyjprogrammer@gmail.com>
2021-11-08 11:54:30 -0500

    set up db layer

---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    //entities are models that get stored in the database
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
