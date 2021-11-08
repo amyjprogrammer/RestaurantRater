86bc9097c1e1f6aa923869a687d62711b56eae95
amyjprogrammer <amyjprogrammer@gmail.com>
2021-11-08 13:16:13 -0500

    Setup Restaurant Index

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,11 @@ namespace RestaurantRater.Models
     public class Restaurant
     {
         public int RestaurantID { get; set; }
+
+        [DisplayName("Restaurant Name")]
         public string Name { get; set; }
+
+        [DisplayName("Location")]
         public string Address { get; set; }
         public int Rating { get; set; }
     }
